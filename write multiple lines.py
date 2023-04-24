cd904d69b971573561028b631e20dc6e3f153410
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-24 21:43:00 +0800

    test run 1

--- write multiple lines.py
+++ write multiple lines.py
@@ -3,19 +3,17 @@
 # Write a method in python to write multiple line of text contents into a text file mylife.txt
 
 # Open mylife.txt (write)
-with open("mylife.txt", 'w') as input_file:
+with open("mylife.txt", "w") as input_file:
     
     # Create a loop
     while True:
-
         # Ask for user input
-        user_input = input =("Enter line: ")
+        user_input = input("Enter line: ")
 
         # write the input to mylife.txt
         input_file.write(str(user_input))
-        """"""
     
-    # add more lines
+    # ask the user if he/she wants to add more lines
     # write the input to mylife.txt
     # if yes
     # continue
