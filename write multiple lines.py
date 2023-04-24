a4456e4dd549b2aea3778299e941771ed934fd94
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-24 21:33:45 +0800

    create and open mylife.txt

--- write multiple lines.py
+++ write multiple lines.py
@@ -3,12 +3,15 @@
 # Write a method in python to write multiple line of text contents into a text file mylife.txt
 
 # open mylife.txt (write)
-# create a loop
-# ask for user input
-# write the input to mylife.txt
-# add more lines
-# write the input to mylife.txt
-# if yes
-	# continue
-# if no
-	# exit
+with open("mylife.txt", 'w') as input_file:
+
+    """"""
+    # create a loop
+    # ask for user input
+    # write the input to mylife.txt
+    # add more lines
+    # write the input to mylife.txt
+    # if yes
+    # continue
+    # if no
+    # exit
