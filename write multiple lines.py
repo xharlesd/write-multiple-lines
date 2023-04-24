026b530494492e99b03f989a29c754549ec9d508
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-24 21:37:27 +0800

    ask for user input

--- write multiple lines.py
+++ write multiple lines.py
@@ -2,12 +2,18 @@
 # Object Oriented Programming | Assignment 4 - Problem 2
 # Write a method in python to write multiple line of text contents into a text file mylife.txt
 
-# open mylife.txt (write)
+# Open mylife.txt (write)
 with open("mylife.txt", 'w') as input_file:
+    
+    # Create a loop
     while True:
+
+        # Ask for user input
+        user_input = input =("Enter line: ")
+        
         """"""
-    # create a loop
-    # ask for user input
+    
+        
     # write the input to mylife.txt
     # add more lines
     # write the input to mylife.txt
