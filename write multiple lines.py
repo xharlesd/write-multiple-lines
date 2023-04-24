3773d00005fcc850fc40595f5ea6b110c3b0ee46
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-24 21:29:45 +0800

    pseudocode

---
+++ write multiple lines.py
@@ -0,0 +1,14 @@
+# Bernido, Charles David P. | BSCPE 1-5
+# Object Oriented Programming | Assignment 4 - Problem 2
+# Write a method in python to write multiple line of text contents into a text file mylife.txt
+
+# open mylife.txt (write)
+# create a loop
+# ask for user input
+# write the input to mylife.txt
+# add more lines
+# write the input to mylife.txt
+# if yes
+	# continue
+# if no
+	# exit
